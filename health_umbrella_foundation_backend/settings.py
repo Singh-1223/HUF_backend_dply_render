9613fed5a14088d685290506aa5c53a14a59c679
Lovepreet singh <126049926+Singh-1223@users.noreply.github.com>
2024-09-21 12:42:07 +0530

    Update settings.py

--- health_umbrella_foundation_backend/settings.py
+++ health_umbrella_foundation_backend/settings.py
@@ -134,29 +134,29 @@ WSGI_APPLICATION = "health_umbrella_foundation_backend.wsgi.application"
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'your_database_name',
-        'USER': 'huf_user',
-        'PASSWORD': 'your_password',
-        'HOST': 'dpg-crbfil5svqrc73evuss0-a.oregon-postgres.render.com',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'your_database_name',
+#         'USER': 'huf_user',
+#         'PASSWORD': 'your_password',
+#         'HOST': 'dpg-crbfil5svqrc73evuss0-a.oregon-postgres.render.com',
+#         'PORT': '5432',
+#     }
+# }
 
-# if ENVIRONMENT == 'development':
-#     DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#       }
-#     }
-# else:
-#     import dj_database_url
-#     DATABASES = {
-#         "default": dj_database_url.parse(env("DATABASE_URL"))
-#     }
+if ENVIRONMENT == 'development':
+    DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+      }
+    }
+else:
+    import dj_database_url
+    DATABASES = {
+        "default": dj_database_url.parse(env("DATABASE_URL"))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
